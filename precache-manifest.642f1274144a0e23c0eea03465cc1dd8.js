4e5ee2f1f99866f23c901ffec681e5b4318ef588
swoyam1 <swoyam1.pant@gmail.com>
2023-07-10 00:24:45 +0530

    Updates

---
+++ precache-manifest.642f1274144a0e23c0eea03465cc1dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77142753b2dd1b71345205860512eff0",
+    "url": "/image-search-app/index.html"
+  },
+  {
+    "revision": "c5ec180ae15e4101d6a2",
+    "url": "/image-search-app/static/css/main.ba1c0e40.chunk.css"
+  },
+  {
+    "revision": "2ddf5f821f6795fd8313",
+    "url": "/image-search-app/static/js/2.51ecf40a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/image-search-app/static/js/2.51ecf40a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ec180ae15e4101d6a2",
+    "url": "/image-search-app/static/js/main.e1e77afb.chunk.js"
+  },
+  {
+    "revision": "aa0f77d4f1dd56fbf6fc",
+    "url": "/image-search-app/static/js/runtime-main.5d6cff6d.js"
+  }
+]);
